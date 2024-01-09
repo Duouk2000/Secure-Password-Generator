03af87d19f97e7344b9ee5b3f5e9b2142cd5568e
Duouk2000 <duouk2000@gmail.com>
2024-01-09 00:12:43 +0000

    added remaining code to generate a random password from the array using the user's input

--- script.js
+++ script.js
@@ -88,36 +88,72 @@ var upperCasedCharacters = [
   'Z'
 ];
 
-
 // Function to prompt user for password options
 function getPasswordOptions() {
-    var characterCount = prompt("How many characters do you need the password to be?");
-    var length = characterCount;
+  var characterCount = prompt("How many characters do you need the password to be?");
+  var length = characterCount;
 
-    if (length < 8 || length > 128 || isNaN(length)) {
-      alert("You must select between 8 and 128 numeric characters");
-      return;
+  if (length < 8 || length > 128 || isNaN(length)) {
+    alert("Please select a number between 8 and 128 for the password length.");
+    return;
   }
-
+// confirms if you want to include the below character types in your password
   var includeSpecialChars = confirm("Do you want to include special characters?");
   var includeNumbers = confirm("Do you want to include numbers?");
   var includeLowercase = confirm("Do you want to include lowercase letters?");
   var includeUppercase = confirm("Do you want to include uppercase letters?");
-
+// checks if the user has selected at least one character type - informs user if they have not
   if (!includeSpecialChars && !includeNumbers && !includeLowercase && !includeUppercase) {
     alert("You must select at least one character type for the password.");
     return;
   }
-}
+// stores the users choice using boolean
+  var passwordOptions = {
+    length: length,
+    includeSpecialChars: includeSpecialChars,
+    includeNumbers: includeNumbers,
+    includeLowercase: includeLowercase,
+    includeUppercase: includeUppercase
+  };
 
+  return passwordOptions;
+}
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+  var randomIndex = Math.floor(Math.random() * arr.length);
+  return arr[randomIndex];
 }
-
 // Function to generate password with user input
 function generatePassword() {
+  var options = getPasswordOptions();
 
+  if (!options) {
+    return "";
+  }
+
+  var availableCharacters = [];
+
+  if (options.includeSpecialChars) {
+    availableCharacters = availableCharacters.concat(specialCharacters);
+  }
+  if (options.includeNumbers) {
+    availableCharacters = availableCharacters.concat(numericCharacters);
+  }
+  if (options.includeLowercase) {
+    availableCharacters = availableCharacters.concat(lowerCasedCharacters);
+  }
+  if (options.includeUppercase) {
+    availableCharacters = availableCharacters.concat(upperCasedCharacters);
+  }
+
+  var generatedPassword = "";
+
+  for (var i = 0; i < options.length; i++) {
+    var randomChar = getRandom(availableCharacters);
+    generatedPassword += randomChar;
+  }
+
+  return generatedPassword;
 }
 
 // Get references to the #generate element
@@ -127,7 +163,6 @@ var generateBtn = document.querySelector('#generate');
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
-
   passwordText.value = password;
 }
 
